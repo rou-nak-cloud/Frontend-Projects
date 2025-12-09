5f5c8f730835c7af311d94c6437800713a3f0d12
rou-nak-cloud <github.rounak.19@gmail.com>
2025-12-09 07:57:09 +0530

    page1 js | duo

---
+++ DuoWork/script.js
@@ -0,0 +1,37 @@
+(function lenis(){
+    // Initialize Lenis
+const lenis = new Lenis({
+    duration: 2,
+});
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+})();
+
+// gsap
+gsap.registerPlugin(ScrollTrigger)
+// page 1 gsap
+var tl1 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page1 .pageCenter h1",
+        scroller: "body",
+        // markers: true,
+        start: "top 20%",
+        end: "top 0",
+        scrub: 3,        
+    },
+})
+tl1.to(".page1 .pageCenter .textFir", {
+    x: -100,
+},"anim") // same name for everyone to start at once together
+tl1.to(".page1 .pageCenter .textSec", {
+    x: 100,
+},"anim")
+tl1.to(".page1 .videoContainer ", {
+    width: "90%"
+},"anim")    
