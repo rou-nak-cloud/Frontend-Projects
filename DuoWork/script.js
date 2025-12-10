29c29de21096d7d1f39a858363fb9c8ff134ecf2
rou-nak-cloud <github.rounak.19@gmail.com>
2025-12-10 22:17:35 +0530

    cursor movement | duo

--- DuoWork/script.js
+++ DuoWork/script.js
@@ -13,6 +13,36 @@ function raf(time) {
 requestAnimationFrame(raf);
 })();
 
+function cursor(){
+    // cursor
+const cursor = document.querySelector("#cursor");
+const cursorText = document.getElementById("cursorText");
+const video = document.querySelector(".videoContainer video");
+
+// Move cursor
+document.addEventListener("mousemove", (e) => {
+  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px) scale(1)`;
+})
+// Cursor grows when entering video area
+video.addEventListener("mouseenter", () => {
+  cursor.style.width = "8vw";
+  cursor.style.height = "1.5vw";
+  cursor.style.borderRadius = "30px";
+  cursor.style.transform += " scale(1.1)";
+  cursorText.textContent = "Sound On";
+  cursorText.style.opacity = 1;
+});
+
+// Return to normal on leave
+video.addEventListener("mouseleave", () => {
+  cursor.style.width = "1vw";
+  cursor.style.height = "1vw";
+  cursor.style.borderRadius = "50%";
+  cursorText.style.opacity = 0;
+});
+}
+cursor();
+
 // gsap
 gsap.registerPlugin(ScrollTrigger)
 // page 1 gsap
