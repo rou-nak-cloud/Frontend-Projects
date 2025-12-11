d0d72978b11a134c70bd8f6c0e5b52241dfe73e8
rou-nak-cloud <github.rounak.19@gmail.com>
2025-12-11 22:31:43 +0530

    clients page 4| duo

--- DuoWork/script.js
+++ DuoWork/script.js
@@ -94,3 +94,60 @@ var tl3 = gsap.timeline({
 tl3.to(".page3", {
     backgroundColor: "#000"
 })
+
+
+
+function imageDisplay(){
+        var elemC = document.querySelector("#elemContainer");
+    var fixedImg = document.querySelector("#fixedImage");
+     var elems = document.querySelectorAll(".box"); // asAll gives nodeList similar as array
+
+    //  Desktop mode
+    // show / hide
+    elemC.addEventListener("mouseenter", function () {
+        fixedImg.style.display = "block";
+    });
+    elemC.addEventListener("mouseleave", function () {
+        fixedImg.style.display = "none";
+    });
+
+    // change image
+    elems.forEach(function (e) {  // e-> each element and maps until all elements
+        e.addEventListener("mouseenter", function () {
+            var image = e.getAttribute("data-image");
+            fixedImg.style.backgroundImage = `url(${image})`;
+        });
+    }); // static ends
+
+      // ----------- FOLLOW MOUSE WITH EASING + LIMITS ----------
+    let x = 0, y = 0;
+    let mx = 0, my = 0;
+
+    document.addEventListener("mousemove", function (e) {
+        x = e.clientX + 20; // offset from cursor
+        y = e.clientY + 20;
+    });
+
+    function animate() {
+        mx += (x - mx) * 0.1;
+        my += (y - my) * 0.1;
+
+        // prevent image going outside right/bottom edges
+        const maxX = window.innerWidth - fixedImg.offsetWidth - 20;
+        const maxY = window.innerHeight - fixedImg.offsetHeight - 20;
+
+        let finalX = Math.min(mx, maxX);
+        let finalY = Math.min(my, maxY);
+
+        // prevent image from going above/left edges
+        finalX = Math.max(0, finalX);
+        finalY = Math.max(0, finalY);
+
+        fixedImg.style.transform = `translate(${finalX}px, ${finalY}px)`;
+
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+}
+imageDisplay();
