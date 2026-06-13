d52a48902c882ff34153c7194376e4c9f4c4bba4
rou-nak-cloud <github.rounak.19@gmail.com>
2026-06-13 17:37:10 +0530

    Game Project Player Logic

--- Tic-Tac-Toe/src/App.jsx
+++ Tic-Tac-Toe/src/App.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import Player from "./components/Player";
+
 export default function App() {
+  const [players, setPlayers] = useState({
+    X: "Player 1",
+    O: "Player 2",
+  });
+  // players is used to check the winner based on symbol
+
+  function handlePlayerChangeName(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName,
+      };
+    });
+  }
   return (
     <>
-      <div></div>
+      <div id="game-container" className="highlight-player">
+        <div id="players">
+          <Player
+            initialPlayer={"Player 1"}
+            Symbol={"X"}
+            onChangeName={handlePlayerChangeName}
+          />
+          <Player
+            initialPlayer={"Player 2"}
+            Symbol={"O"}
+            onChangeName={handlePlayerChangeName}
+          />
+        </div>
+      </div>
     </>
   );
 }
---
+++ Tic-Tac-Toe/src/components/Player.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function Player({ initialPlayer, Symbol, onChangeName }) {
+  const [playerName, setPlayerName] = useState(initialPlayer);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((isEditing) => !isEditing);
+    onChangeName(Symbol, playerName);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <>
+      <li>
+        <span className="player">
+          {isEditing ? (
+            <input
+              type="text"
+              required
+              value={playerName}
+              onChange={handleChange}
+            />
+          ) : (
+            <span className="player-name">{playerName}</span>
+          )}
+          <span className="player-symbol">{Symbol}</span>
+        </span>
+        <button type="button" onClick={handleEditClick}>
+          {isEditing ? "Save" : "Edit"}
+        </button>
+      </li>
+    </>
+  );
+}
